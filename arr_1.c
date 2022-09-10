fe87c6e08f3f9760f7909dfd08a0a101aa5dd099
bkabhilash0 <bkabhilash0@gmail.com>
2022-09-11 00:00:25 +0530

    Added the Determinant Logic

--- arr_1.c
+++ arr_1.c
@@ -1,1 +1,15 @@
 // Demo Storage Classes in C
+
+#include <stdio.h>
+
+int global = 10;
+
+void display(){
+
+}
+
+void main(){
+    printf("The value of Global Variable is %d\n", global);
+    printf("Hello World!\n");
+    
+}
--- arr_6.c
+++ arr_6.c
@@ -80,8 +80,68 @@ void transpose(int a[SIZE][SIZE])
     display(result);
 }
 
-void determinant(int a[SIZE][SIZE]){
-    int i,j,result;
+// Function to get cofactor of mat[p][q] in temp[][]. n is
+// current dimension of mat[][]
+void getCofactor(int mat[SIZE][SIZE], int temp[SIZE][SIZE], int p,
+                 int q, int n)
+{
+    int i = 0, j = 0;
+
+    // Looping for each element of the matrix
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            //  Copying into temporary matrix only those
+            //  element which are not in given row and
+            //  column
+            if (row != p && col != q)
+            {
+                temp[i][j++] = mat[row][col];
+
+                // Row is filled, so increase row index and
+                // reset col index
+                if (j == n - 1)
+                {
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+/* Recursive function for finding determinant of matrix.
+   n is current dimension of mat[][]. */
+int determinantOfMatrix(int mat[SIZE][SIZE], int n)
+{
+    int D = 0; // Initialize result
+
+    //  Base case : if matrix contains single element
+    if (n == 1)
+        return mat[0][0];
+
+    int temp[SIZE][SIZE]; // To store cofactors
+
+    int sign = 1; // To store sign multiplier
+
+    // Iterate for each element of first row
+    for (int f = 0; f < n; f++)
+    {
+        // Getting Cofactor of mat[0][f]
+        getCofactor(mat, temp, 0, f, n);
+        D += sign * mat[0][f] * determinantOfMatrix(temp, n - 1);
+
+        // terms are to be added with alternate sign
+        sign = -sign;
+    }
+
+    return D;
+}
+
+void determinant(int a[SIZE][SIZE])
+{
+    int i, j, result;
 }
 
 void main()
@@ -97,5 +157,5 @@ void main()
     printf("-------------------\n");
     transpose(matrix_1);
     printf("-------------------\n");
-    determinant(matrix_1);
+    printf("%d",determinantOfMatrix(matrix_1,SIZE));
 }
